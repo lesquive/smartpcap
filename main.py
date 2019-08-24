06b44d953bfc16868b1b9acfb082e3d4077df742
Luis Esquivel <luis-esquivel@Luiss-MacBook-Pro.local>
2019-08-24 14:38:20 -0600

    added layer 3 and layer 2 info

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from scapy.all import *
 from scapy.utils import *
 import time
@@ -6,19 +7,34 @@ start = time.time()
 
 pktnum = 0
 
-pkts = rdpcap ('20thousand.pcapng')
+pkts = rdpcap ('test.pcapng')
+
+start2 = time.time()
 
 for pkt in pkts:
-    eth_src = pkt[Ether].src 
-    eth_dst = pkt[Ether].dst
-    pktnum = pktnum + 1
+    pktnum += 1
+    try:
+        eth_src = pkt[Ether].src 
+        eth_dst = pkt[Ether].dst
+        eth_type = pkt[Ether].type
+        ip_src = pkt[IP].src
+        ip_dst = pkt[IP].dst
+        ip_proto = pkt[IP].proto
 
-    print("")
-    print("Packet number {}".format(pktnum))
-    print("The source MAC is: {}".format(eth_src))
-    print("The source MAC is: {}".format(eth_dst))
+        print("")
+        print("Packet number {}".format(pktnum))
+        print("Source MAC: {}".format(eth_src))
+        print("Destination MAC: {}".format(eth_dst))
+        print("Ethernet type: {}".format(eth_type))
+        print("Source IP: {}".format(ip_src))
+        print("Destination IP: {}".format(ip_dst))
+        print("Layer 4 protocol: {}".format(ip_proto))
 
+    except:
+        print("An exception occurred")
+
+end2 = time.time()
 end = time.time()
 print("")
+print("total analysis time: {}".format(end2 - start2))
 print("total execution time: {}".format(end - start))
-print(type(PcapReader('20thousand.pcapng')))
