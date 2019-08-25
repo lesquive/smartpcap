62a05732bc54760173a68da38e901732faad5d46
Esquivel <luis.esquivel4@amer.sykes.com>
2019-08-25 12:33:46 -0600

    made a real main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+Project: Smart PCAP
+Program name: smartpcap
+Author: Luis Jose Esquivel Blanco, lesquive@cisco.com
+Date created: 20190825
+Purpose: Make a program that facilitates packet analysis 
+'''
+
+def main():
+    pass
+
+if __name__== "__main__":
+  main()
