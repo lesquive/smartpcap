128652cb4679e0dea658f6c5640eb5e6ab88e2a8
Esquivel <luis.esquivel4@amer.sykes.com>
2019-08-25 18:59:07 -0600

    added DHCP options

--- dhcp.py
+++ dhcp.py
@@ -7,14 +7,22 @@ start = time.time()
 
 pktnum = 0
 
-pkts = rdpcap ('110kpackets.pcapng')
+pkts = rdpcap ('test.cap')
 
 for pkt in pkts:
 
     pktnum += 1
 
     try:
+        print(hex(pkt['BOOTP'].xid))
         print(pkt['DHCP options'].options[0])
+        print(pkt['DHCP options'].options[1])
+        print(pkt['DHCP options'].options[2])
+        print(pkt['DHCP options'].options[3])
+        print(pkt['DHCP options'].options[4])
+        print(pkt['DHCP options'].options[5])
+        print("")
+
 
     except:
         pass
