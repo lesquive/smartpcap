55a90105571f1dd91a75126bf84d861919719d60
Luis Esquivel <luis-esquivel@Luiss-MacBook-Pro.local>
2019-09-16 15:43:22 -0600

    latest updates to date before changing CSS to fix bug

--- web-main.py
+++ web-main.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, session
 from flask import * 
+from flask_session import Session
+from scapy.all import *
+from scapy.utils import *
+
+dhcp = []
+dns = []
+icmp =[]
+woho = []
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 @app.route("/")
@@ -12,7 +23,22 @@ def index():
 @app.route('/success', methods = ['GET','POST'])  
 def success():  
     if request.method == 'POST':  
+        session['dhcp'] = []
         f = request.files['file']  
-        f.save(f.filename)  
-    return render_template("success.html")   
 
+        session["pkts"] = rdpcap (f)
+
+        for pkt in session["pkts"]:
+
+            try:
+                # session['dhcp-confirm'] = session['dhcp'].append(hex(pkt['BOOTP'].xid))
+                session['dhcp-confirm'] = pkt['BOOTP'].xid
+
+                if session['dhcp-confirm']:
+                    session['dhcp'].append(pkt)
+                    
+
+            except:
+                pass
+
+    return render_template("success.html", packets=session['dhcp'])
