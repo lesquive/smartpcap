b5c8475671a272dfac08879bd379b9af0a4a2780
Luis Esquivel <luis-esquivel@Luiss-MacBook-Pro.local>
2019-09-17 16:38:39 -0600

    Packet font is fixed as expected

--- web-main.py
+++ web-main.py
@@ -23,22 +23,61 @@ def index():
 @app.route('/success', methods = ['GET','POST'])  
 def success():  
     if request.method == 'POST':  
-        session['dhcp'] = []
+        
+        #DHCP Related variables:
+        # session['dhcp'] = []
+        session['dhcp_discover'] = set()
+        session['dhcp_ack'] = set()
+        session['dora'] = []
+        session['doraFailedCount'] = 0
+        session['doraFailedList'] = []
+        session['ipList'] = []
+
+        #DNS Related variables:
+        session['dns'] = []
         f = request.files['file']  
 
         session["pkts"] = rdpcap (f)
 
+        pktnum = 0
+
         for pkt in session["pkts"]:
 
+            pktnum += 1
+
             try:
-                # session['dhcp-confirm'] = session['dhcp'].append(hex(pkt['BOOTP'].xid))
-                session['dhcp-confirm'] = pkt['BOOTP'].xid
 
-                if session['dhcp-confirm']:
-                    session['dhcp'].append(pkt)
-                    
+                if pkt['BOOTP'].xid:
+                    dhcpId = (hex(pkt['BOOTP'].xid))
+                    option1 = (pkt['DHCP options'].options[0][1])
+                    srcmac = (pkt['Ether'].src)
 
+                    if option1 == 1:
+                        session['dhcp_discover'].add(dhcpId)
+                        session['dora'].append("DHCP Discover detected from {}, with transaction ID: {}, packet number: {}".format(srcmac, dhcpId, pktnum))
+                    elif option1 == 2:
+                        session['dora'].append("DHCP Offer detected from {}, with transaction ID: {}, packet number: {}".format(srcmac, dhcpId, pktnum))                   
+                    elif option1 == 3:
+                        session['dora'].append("DHCP Request detected from {}, with transaction ID: {}, packet number: {}".format(srcmac, dhcpId, pktnum))
+                    elif option1 == 5:
+                        session['dhcp_ack'].add(dhcpId)
+                        session['dora'].append("DHCP ACK detected from {}, with transaction ID: {}, packet number: {}".format(srcmac, dhcpId, pktnum)) 
+                        
             except:
                 pass
 
-    return render_template("success.html", packets=session['dhcp'])
+            try: 
+                
+                if pkt['DNS']:
+                    session['dns'].append(pkt)
+                    
+            except:
+                pass
+
+
+        for i in session['dhcp_discover']:
+            if i not in session['dhcp_ack']:
+                session['doraFailedList'].append("DHCP ACK missing for transaction ID: {}".format(i))
+                session['doraFailedCount'] +=1
+
+    return render_template("success.html", dhcp=session['dora'], doraFailedList=session['doraFailedList'], doraFailedCount=session['doraFailedCount'], dns=session['dns'])
